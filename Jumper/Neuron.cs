798f18ac2c6ced79e2c344f59c6eaabf955d6c7a
andreas <Andreas.Zwander@gmail.com>
2026-01-29 19:16:05 +0100

    Refactor Neuron.cs to use `var` in loop variables for consistency

--- Jumper/Neuron.cs
+++ Jumper/Neuron.cs
@@ -9,22 +9,22 @@ public class Neuron
     public Neuron(int inputCount, Random rng)
     {
         Weights = new double[inputCount];
-        for (int i = 0; i < inputCount; i++)
+        for (var i = 0; i < inputCount; i++)
             Weights[i] = rng.NextDouble() * 2 - 1;
         Bias = rng.NextDouble() * 2 - 1;
     }
 
     public double CalculateSum(double[] inputs)
     {
-        double sum = Bias;
-        for (int i = 0; i < Weights.Length; i++)
+        var sum = Bias;
+        for (var i = 0; i < Weights.Length; i++)
             sum += inputs[i] * Weights[i];
         return sum;
     }
 
     public void UpdateWeights(double delta, double learningRate, double[] inputs)
     {
-        for (int i = 0; i < Weights.Length; i++)
+        for (var i = 0; i < Weights.Length; i++)
             Weights[i] += learningRate * delta * inputs[i];
         Bias += learningRate * delta;
     }
