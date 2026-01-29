af3cbdd2b6c9068343d7fa3faf4b61b666fe774e
andreas <Andreas.Zwander@gmail.com>
2026-01-29 20:17:24 +0100

    Update EvolutionManager: include Speed in network input and revise fitness calculation logic

--- Jumper/EvolutionManager.cs
+++ Jumper/EvolutionManager.cs
@@ -6,16 +6,14 @@ public class EvolutionManager
     {
         var game = new JumperGame();
         var frames = 0;
-        
-        // Die KI darf max. 2000 Frames spielen
+
         while (!game.IsDead && frames < 2000)
         {
-            var output = net.Predict([game.ObstacleX, game.PlayerY]);
+            var output = net.Predict([game.ObstacleX, game.PlayerY, game.Speed]);
             game.Update(output[0] > 0.5);
             frames++;
         }
-        
-        // Fitness = Score + wie lange sie überlebt hat
+
         return game.Score * 100 + frames;
     }
 }
