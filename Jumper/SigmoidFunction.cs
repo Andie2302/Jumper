75e34910c51ba49fb5d237675c2b9198af61f8f0
andreas <Andreas.Zwander@gmail.com>
2026-01-29 19:13:01 +0100

    Add SigmoidFunction class implementing IActivationFunction in Jumper project

---
+++ Jumper/SigmoidFunction.cs
@@ -0,0 +1,5 @@
+public class SigmoidFunction : IActivationFunction
+{
+    public double Activate(double x) => 1.0 / (1.0 + Math.Exp(-x));
+    public double Derivative(double output) => output * (1.0 - output);
+}
